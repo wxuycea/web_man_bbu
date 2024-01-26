decca70868b3a978232a98dc6abc20409cdee71e
년수 <numbbvi@naver.com>
2024-01-26 20:02:32 +0900

    2024.01.25 commit

--- register-process.php
+++ register-process.php
@@ -3,6 +3,8 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+session_start();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $id = $_POST['id'];
@@ -19,19 +21,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
 
+    $_SESSION['idCheck'] = true;
+    $_SESSION['nicknameCheck'] = true;
+
     if ($pw != $confirm_pw) {
         echo "<script>alert('비밀번호가 일치하지 않습니다.');</script>";
-        echo "<script>location.href = 'templates/register.html';</script>";
+        echo "<script>window.history.go(-1);</script>";
     } else {
-        $query = "INSERT INTO user (userName, id, passwd, nickname, question, answer) VALUES('$username', '$id', '$pw', '$nickname', '$question', '$answer')";
-        $mysqli->query($query);
-
-        if ($mysqli->affected_rows > 0) {
-            echo "<script>alert('회원가입에 성공하였습니다.');</script>";
-            echo "<script>location.href = 'templates/login.html';</script>";
+        if ($_SESSION['idCheck'] && $_SESSION['nickname']) {
+            $query = "INSERT INTO user (userName, id, passwd, nickname, question, answer) VALUES('$username', '$id', '$pw', '$nickname', '$question', '$answer')";
+            $mysqli->query($query);
+            if ($mysqli->affected_rows > 0) {
+                unset($_SESSION['id_duplicate']);
+                unset($_SESSION['nickname_duplicate']);
+                echo "<script>alert('회원가입에 성공하였습니다.');</script>";
+                echo "<script>location.href = 'templates/login.html';</script>";
+            } else {
+                echo "<script>alert('회원가입에 실패하였습니다. 다시 시도해주세요.');</script>";
+                echo "<script>location.href = 'templates/register.html';</script>";
+            }
         } else {
-            echo "<script>alert('회원가입에 실패하였습니다. 다시 시도해주세요.');</script>";
-            echo "<script>location.href = 'templates/register.html';</script>";
+            echo "<script>alert('중복 확인이 완료되지 않았습니다.');</script>";
+            echo "<script>window.history.go(-1);</script>";
         }
     }
 }
